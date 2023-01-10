4401d6df4841707aa4dbf6944bf6f2538529c50c
daniel <daniel.robert.lewis@gmail.com>
2023-01-10 20:12:42 +0100

    adds button example

--- public/index.js
+++ public/index.js
@@ -1,14 +1,3 @@
-
-jQuery.get("/example_url_param/foo", function(d) {
-  jQuery('#url_param_value').text(d.changed)
-})
-
-
-jQuery.get("/example_query_param?param=bar", function(d) {
-  jQuery('#query_param_value').text(d.changed)
-})
-
-
 // jQuery.post doesn't set the right contentType for Quart, so we gotta do this
 function post(url, data, fn) {
   jQuery.ajax({
@@ -19,6 +8,26 @@ function post(url, data, fn) {
     success: fn
   })
 }
-post("/example_json_post", { param: "baz"}, function(d) {
-  jQuery('#post_param_value').text(d.changed)
+
+
+function update_values() {
+  jQuery.get("/example_url_param/foo", function(d) {
+    jQuery('#url_param_value').text(d.changed)
+  })
+
+
+  jQuery.get("/example_query_param?param=bar", function(d) {
+    jQuery('#query_param_value').text(d.changed)
+  })
+
+
+  post("/example_json_post", { param: "baz"}, function(d) {
+    jQuery('#post_param_value').text(d.changed)
+  })
+}
+
+update_values() // set initial values
+
+jQuery('button.do_update').click(function() { 
+  update_values()
 })
